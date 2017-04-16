d0e1cc76875e331e7ade35239e96bdb0bd055ce8
yuhsuan <frorgr@hotmail.com>
2017-04-16 13:45:59 +0800

    Change path

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     main: './public/src/main.js'
   },
   output: {
-    path: path.join(__dirname + '/public/'),
+    path: path.join(__dirname + '/public'),
     filename: 'bundle.js',
     publicPath: '/public/'
   },
